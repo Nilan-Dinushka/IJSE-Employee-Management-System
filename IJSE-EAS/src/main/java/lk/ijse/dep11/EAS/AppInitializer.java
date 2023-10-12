b70e0f1459c472e8d052bfb2c86d295c9f5b9efd
Nilan Dinushka <nilandinushka5@gmail.com>
2023-10-12 11:12:01 +0530

    Create loginform

---
+++ IJSE-EAS/src/main/java/lk/ijse/dep11/EAS/AppInitializer.java
@@ -0,0 +1,28 @@
+package lk.ijse.dep11.EAS;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        AnchorPane root = FXMLLoader.load(getClass().getResource("/view/LoginForm.fxml"));
+        Scene mainScene = new Scene(root);
+        primaryStage.setScene(mainScene);
+        primaryStage.setTitle("IJSE EAS LOGIN");
+        primaryStage.setResizable(false);
+        primaryStage.show();
+        primaryStage.centerOnScreen();
+
+    }
+}
---
+++ IJSE-EAS/src/main/java/lk/ijse/dep11/EAS/controller/LoginFormController.java
@@ -0,0 +1,4 @@
+package lk.ijse.dep11.EAS.controller;
+
+public class LoginFormController {
+}
